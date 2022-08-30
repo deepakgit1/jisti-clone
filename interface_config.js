56088060e4000e5b03b48344565c21dc32aba224
Deepak Gaidhane <deepak@devbeeit.com>
2022-08-30 13:44:18 +0530

    make it better

--- interface_config.js
+++ interface_config.js
@@ -8,7 +8,7 @@
  * config.js, and no new options should be added here.
  */
 
-export var interfaceConfig = {
+var interfaceConfig = {
     APP_NAME: 'Jitsi Meet',
     AUDIO_LEVEL_PRIMARY_COLOR: 'rgba(255,255,255,0.4)',
     AUDIO_LEVEL_SECONDARY_COLOR: 'rgba(255,255,255,0.2)',
